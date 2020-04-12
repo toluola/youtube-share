3eb4b10ad6db89c7e4f903702db98094420ae814
toluola <toluola7@gmail.com>
2020-04-11 17:36:19 -0700

    add signin and signup route

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,16 @@
+class AuthenticationController < ApplicationController
+    skip_before_action :authorize_user, only: :authenticate
+    include Response
+    include ExceptionHandler
+  
+    def authenticate
+      auth = AuthenticateUser.new(accept_params[:username], accept_params[:password]).call
+      json_response({ token: auth }, "User logged in Successfully")
+    end
+  
+    private
+  
+    def accept_params
+      params.permit(:username, :password)
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
-class UsersController < ApplicationController
-end
+class UserController < ApplicationController
+    skip_before_action :authorize_user, only: :create
+    include Response
+    include ExceptionHandler
+  
+    def create
+      create_user = User.create!(username: accept_params[:username],
+                                      password: accept_params[:password])
+      json_response(create_user.slice(:id, :name, :username, :created_at, :updated_at),
+                    "User created successfully",
+                    :created)
+    end
+  
+    private
+  
+    def accept_params
+      params.permit(:username, :password)
+    end
+  end

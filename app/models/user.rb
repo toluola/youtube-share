51807c2304680d01a77efc19a85941e88ef4d02a
toluola <toluola7@gmail.com>
2020-04-14 03:18:55 -0700

    fix error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :videos
-    validates_presence_of :username, :password_digest
+    validates_presence_of :username, :password
     validates :username, uniqueness: true
 end

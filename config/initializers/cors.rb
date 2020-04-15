6ff8b8de58cd54ffcb85701894ad39c55fa88fe0
toluola <toluola7@gmail.com>
2020-04-15 00:52:30 -0700

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://youtube-share.netlify.com'
+      origins 'https://youtube-share-client.herokuapp.com'
   
       resource '*',
         headers: :any,
